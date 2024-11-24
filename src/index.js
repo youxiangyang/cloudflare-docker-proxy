6a3473ee7281a947ca343a98b2dc85121c0ba2fb
xiangyang <youxiangyang@users.noreply.github.com>
2024-11-24 23:28:24 +0800

    Update index.js
    update

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.you-code.top": dockerHub,
+  "quay.you-code.top": "https://quay.io",
+  "gcr.you-code.top": "https://gcr.io",
+  "k8s-gcr.you-code.top": "https://k8s.gcr.io",
+  "k8s.you-code.top": "https://registry.k8s.io",
+  "ghcr.you-code.top": "https://ghcr.io",
+  "cloudsmith.you-code.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "docker-staging.you-code.top": dockerHub,
 };
 
 function routeByHosts(host) {
